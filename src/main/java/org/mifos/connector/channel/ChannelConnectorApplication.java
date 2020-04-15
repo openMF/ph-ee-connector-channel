ee93360a07d12b8ce35e91dabf53913fe2081b24
Zoltan Nebli <zoltan.nebli@dpc.hu>
2020-04-15 13:11:24 +0200

    add objectmapper extensions

--- src/main/java/org/mifos/connector/channel/ChannelConnectorApplication.java
+++ src/main/java/org/mifos/connector/channel/ChannelConnectorApplication.java
@@ -3,6 +3,8 @@ package org.mifos.connector.channel;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,9 +14,13 @@ public class ChannelConnectorApplication {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper()
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        return objectMapper
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
     public static void main(String[] args) {

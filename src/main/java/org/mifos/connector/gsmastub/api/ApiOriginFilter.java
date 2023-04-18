cde0bb06d79d3f3b4eb63fa6db7f35ec89a01dec
truthfool <ishanranasingh@gmail.com>
2023-04-18 17:59:49 +0530

    added filters for tenant

--- src/main/java/org/mifos/connector/gsmastub/api/ApiOriginFilter.java
+++ src/main/java/org/mifos/connector/gsmastub/api/ApiOriginFilter.java
@@ -4,7 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,15 +18,16 @@ import java.util.Optional;
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-09-27T11:46:46.417Z[GMT]")
 @Component
 public class ApiOriginFilter implements Filter {
-    private Logger logger= LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest req=(HttpServletRequest)request;
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
         String tenant = req.getHeader("Platform-TenantId");
-        Optional<String>tenantVal=Optional.ofNullable(tenant);
-        if(tenantVal.isPresent()){
-            logger.info("Tenant Name is : {}",tenant);
+        Optional<String> tenantVal = Optional.ofNullable(tenant);
+        if (tenantVal.isPresent()) {
+            logger.info("Tenant Name is : {}", tenant);
         }
         HttpServletResponse res = (HttpServletResponse) response;
         res.addHeader("Access-Control-Allow-Origin", "*");

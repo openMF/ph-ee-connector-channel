a716c8ccc4caf710f71e12579d8a9d735b93d87d
truthfool <ishanranasingh@gmail.com>
2023-04-18 17:23:39 +0530

    added filters for tenant

--- src/main/java/org/mifos/connector/gsmastub/api/ApiOriginFilter.java
+++ src/main/java/org/mifos/connector/gsmastub/api/ApiOriginFilter.java
@@ -1,14 +1,26 @@
 package org.mifos.connector.gsmastub.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-09-27T11:46:46.417Z[GMT]")
 public class ApiOriginFilter implements Filter {
+    private Logger logger= LoggerFactory.getLogger(this.getClass());
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req=(HttpServletRequest)request;
+        String tenant = req.getHeader("Platform-TenantId");
+        Optional<String>tenantVal=Optional.ofNullable(tenant);
+        if(tenantVal.isPresent()){
+            logger.debug("Tenant Name is : {}",tenant);
+        }
         HttpServletResponse res = (HttpServletResponse) response;
         res.addHeader("Access-Control-Allow-Origin", "*");
         res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");

51b0ef867e0887558bfa5ad6f6c1fc23f007e2d3
truthfool <ishanranasingh@gmail.com>
2023-04-18 17:26:29 +0530

    added filters for tenant

--- src/main/java/org/mifos/connector/gsmastub/api/ApiOriginFilter.java
+++ src/main/java/org/mifos/connector/gsmastub/api/ApiOriginFilter.java
@@ -2,6 +2,7 @@ package org.mifos.connector.gsmastub.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,7 @@ import java.io.IOException;
 import java.util.Optional;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-09-27T11:46:46.417Z[GMT]")
+@Component
 public class ApiOriginFilter implements Filter {
     private Logger logger= LoggerFactory.getLogger(this.getClass());
     @Override
@@ -19,7 +21,7 @@ public class ApiOriginFilter implements Filter {
         String tenant = req.getHeader("Platform-TenantId");
         Optional<String>tenantVal=Optional.ofNullable(tenant);
         if(tenantVal.isPresent()){
-            logger.debug("Tenant Name is : {}",tenant);
+            logger.info("Tenant Name is : {}",tenant);
         }
         HttpServletResponse res = (HttpServletResponse) response;
         res.addHeader("Access-Control-Allow-Origin", "*");

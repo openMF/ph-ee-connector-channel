4f9898cedf73c26c8fbddb7567498afc63192339
Ishan Ranasingh <44061541+truthfool@users.noreply.github.com>
2023-06-01 12:49:42 +0530

    Added loggers for tenant in filters (#86)
    * added filters for tenant
    
    * added filters for tenant
    
    * added filters for tenant
    
    * removed tenant from yaml
    
    * addressed review comments
    
    * addressed review comments
    
    * removed check

--- src/main/java/org/mifos/connector/gsmastub/api/ApiOriginFilter.java
+++ src/main/java/org/mifos/connector/gsmastub/api/ApiOriginFilter.java
@@ -1,14 +1,33 @@
 package org.mifos.connector.gsmastub.api;
 
-import javax.servlet.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
+
+import static org.mifos.connector.channel.camel.config.CamelProperties.PLATFORM_TENANT_ID;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-09-27T11:46:46.417Z[GMT]")
+@Component
 public class ApiOriginFilter implements Filter {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain) throws IOException, ServletException {
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        String tenant = req.getHeader(PLATFORM_TENANT_ID);
+        logger.debug("Tenant Name is : {}", tenant);
         HttpServletResponse res = (HttpServletResponse) response;
         res.addHeader("Access-Control-Allow-Origin", "*");
         res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");

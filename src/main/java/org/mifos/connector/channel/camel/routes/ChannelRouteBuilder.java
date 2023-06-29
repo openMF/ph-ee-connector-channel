085d6bee95ec7819400048555324cffb2ee9907a
Ishan Ranasingh <44061541+truthfool@users.noreply.github.com>
2023-06-29 13:31:41 +0530

    [PHEE-321] Null check for custom data (#104)
    * null check for custom data
    
    * utility function for null check
    
    * utility function for null check

--- src/main/java/org/mifos/connector/channel/camel/routes/ChannelRouteBuilder.java
+++ src/main/java/org/mifos/connector/channel/camel/routes/ChannelRouteBuilder.java
@@ -445,7 +445,7 @@ public class ChannelRouteBuilder extends ErrorHandlerRouteBuilder {
                     }//end for loop
                     JSONObject amountObj = body.getJSONObject("amount");
                     String currency = amountObj.getString("currency");
-                    Object customDataObj = body.get("customData");
+                    Object customDataObj = amsUtils.getOrDefault(body, "customData","");
                     String customDataString = String.valueOf(customDataObj);
 
                     logger.info("Final Value for ams : " + finalAmsVal);

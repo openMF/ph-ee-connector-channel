116d2963c6a660546b2c14c51515bced39aa6f6c
dhruvsonagara <dhruvsonagara@gmail.com>
2023-08-04 11:13:15 +0530

    resolved checkstyle error manually

--- src/test/java/org/mifos/connector/channel/TestUtil.java
+++ src/test/java/org/mifos/connector/channel/TestUtil.java
@@ -2,11 +2,13 @@ package org.mifos.connector.channel;
 
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestUtil {
-
+    private Logger log = LoggerFactory.getLogger(TestUtil.class);
     @Test
     public void test1() {
-        System.out.println(UUID.randomUUID().toString());
+        log.info(" {}",UUID.randomUUID().toString());
     }
 }

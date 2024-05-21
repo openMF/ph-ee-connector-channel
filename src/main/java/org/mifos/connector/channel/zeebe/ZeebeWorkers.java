819f086cd2912cd90ca26d109157cb1088a0d412
Anover000 <ankit@fynarfin.io>
2024-05-22 02:01:50 +0530

    test only

--- src/main/java/org/mifos/connector/channel/zeebe/ZeebeWorkers.java
+++ src/main/java/org/mifos/connector/channel/zeebe/ZeebeWorkers.java
@@ -218,6 +218,7 @@ public class ZeebeWorkers {
         zeebeClient.newWorker().jobType("zeebe-consistency-worker").handler((client, job) -> {
             logger.info("Job '{}' started from process '{}' with key {}", job.getType(), job.getBpmnProcessId(), job.getKey());
             Map<String, Object> variables = job.getVariablesAsMap();
+            variables.replace("message", "New World");
             variables.put("Worker message", "Hey There");
             client.newCompleteCommand(job.getKey()).variables(variables).send().join();
         }).name("zeebe-consistency-worker").maxJobsActive(workerMaxJobs).open();
